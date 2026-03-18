a0d24b602cbdb6e762159bc2aeb4f2e47c48697e
erlan <erland.handegard@outlook.com>
2026-03-18 13:46:11 +0100

    All koden er satt opp og alt funker. Skal vider epå vindu nå.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "include/window.h"
+
+int main() {
+    
+    print("Hello");
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
---
+++ src/window.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print(std::string setning){
+    std::cout << setning << std::endl; 
+};
