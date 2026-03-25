668dedf0e375cae67b2dc289413a680236f5c212
erlan <erland.handegard@outlook.com>
2026-03-25 09:07:09 +0100

    Samme som forige commit

--- src/window.cpp
+++ src/window.cpp
@@ -3,7 +3,7 @@
 #include "include/window.h"
 
 GameWindow::GameWindow(TDT4102::Point windowPosition, TDT4102::Point startingDimensions, const std::string& title):
-    TDT4102::AnimationWindow(windowPosition.x, windowPosition.y, startingDimensions.x, startingDimensions.y, title = "Game Window")
+    AnimationWindow(windowPosition.x, windowPosition.y, startingDimensions.x, startingDimensions.y, title = "Game Window")
 {
     
 }
