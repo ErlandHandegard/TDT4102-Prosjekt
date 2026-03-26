f252c7099a1934525520afc6ab24e6de390d6ce4
erlan <erland.handegard@outlook.com>
2026-03-26 12:45:10 +0100

    La til alle oppdateringen til vinduet

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include "include/worlds.h"
 
 int main() {
-    GameWindow gameWindow({100, 100}, {1024, 768}, "Game window");
+    GameWindow gameWindow({100, 100}, {9*32, 4*32}, "Game window");
 
     World world1("eworlds/firstWorld.txt");
 
     while(!gameWindow.should_close()){
-        gameWindow.updateWindow(*world1);
+        gameWindow.updateWindowPosition(world1);
+        gameWindow.updateWindow(world1);
         gameWindow.next_frame();
     }
     return 0;
