6fba55bf464fbcb7ef02858c9b067d21f128438a
erlan <erland.handegard@outlook.com>
2026-04-04 19:33:03 +0200

    Prøver å implementere bevegelse nå.

--- include/player.h
+++ include/player.h
@@ -9,10 +9,12 @@ class Player {
     private:
         int health;
         TDT4102::Point position;
+        TDT4102::Point gridPosition; 
         TDT4102::Point velocity;
         TDT4102::Point acceleration;
     public:
         Player(TDT4102::Point strartingPosition); // Må laste inn ett bilde av karakteren og sette det som sprite.
+        
         int getHealth() const { return health; }
         TDT4102::Point getPosition() const { return position; }
 
--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,6 @@
 #include "include/player.h"
 #include "include/window.h"
+#include "include/worlds.h"
 
 Player::Player(TDT4102::Point strartingPosition){
     this -> health = 400;
@@ -9,17 +10,24 @@ Player::Player(TDT4102::Point strartingPosition){
 }
 
 void Player::move(const World& world, const GameWindow& gameWindow) {
-    if (gameWindow.is_key_down(KeyboardKey::A)) {
-        position.x -= 5; // Move left
-    }
-        if (gameWindow.is_key_down(KeyboardKey::D)) {
-        position.x += 5; // Move right
-    }
-    if (gameWindow.is_key_down(KeyboardKey::W)) {
-        position.y -= 5; // Move up
-    }
-    if (gameWindow.is_key_down(KeyboardKey::S)) {
-        position.y += 5; // Move down
-    }
+    this -> gridPosition.x = position.x / 32; // Dette er posisojonen til øvre venstre hjørne av spilleren
+    this -> gridPosition.y = position.y / 32; 
+    std::vector<std::vector<bool>> collitionMatrix = world.getCollitionMatrix();
+    
 
+    //Hopping
+    if (gameWindow.is_key_down(KeyboardKey::SPACE)) {
+        this -> velocity.y = -10; // Jumping velocity
+    }
+    // Bevegelse frem og tilbake
+    if (gameWindow.is_key_down(KeyboardKey::A)) {
+        this -> velocity.x = -5; // Move left
+    }
+    if (gameWindow.is_key_down(KeyboardKey::D)) {
+        this -> velocity.x = 5; // Move right
+    }
+    this -> velocity.y += this -> acceleration.y;
+    this -> velocity.x += this -> acceleration.x;
+    this -> position.y += this -> velocity.y;
+    this -> position.x += this -> velocity.x;
 }
