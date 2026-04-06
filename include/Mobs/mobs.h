fd97fa8ef87f4c167df1d2221252a1975b37af38
FANGORN-no <80514112+FANGORN-no@users.noreply.github.com>
2026-04-06 18:00:52 +0200

    starter på mobs

--- include/Mobs/mobs.h
+++ include/Mobs/mobs.h
@@ -5,8 +5,17 @@
 
 class MobileEntities{
     private:
-        int hitPoints;
+        int health;
         TDT4102::Point position; 
+        TDT4102::Point gridPosition; 
+        TDT4102::Point velocity;
+        TDT4102::Point acceleration;
+        TDT4102::Point mobSize;
     public:
         MobileEntities(int type);
+        int getHealth() const { return health; }
+        TDT4102::Point getPosition() const { return position; }
+
+        void move(const World& world, const GameWindow& gameWindow);
+
 };
